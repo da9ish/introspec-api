324033ccef5eaadd26f40e3023ca8192b0f06cfb
Danish Shah <shah.danish.1996@gmail.com>
2023-01-26 03:28:27 +0400

    build(cors): fix cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,10 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
-    resource "*",
-             headers: :any,
-             expose:  %w[access-token expiry token-type uid client environment-id],
-             methods: %i[get post options delete put]
+    origins "*" # TODO: Fix actual origins
+    resource "*", headers: :any, methods: %i[get post patch put delete options head], expose: %w[Authorization], max_age: 600
   end
 end
