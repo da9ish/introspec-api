d93a465454a709c42efd740f5619181dc14b6461
Danish Shah <shah.danish.1996@gmail.com>
2023-01-26 05:40:47 +0400

    build(cors): revert origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*.introspec.app"
+    origins "*"
     resource "*", headers: :any, methods: %i[get post patch put option]
   end
 end
